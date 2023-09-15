38b8496d232e34c1831cb676db6a05c35072e85f
Andy Patari <andy.patari@gmail.com>
2023-09-15 08:48:44 -0400

    passing tests

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,13 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  vals = new Set()
+  for(item of arr) {
+    if(vals.has(item)) {
+      return item
+    }
+    vals.add(item)
+  }
+  return -1
 }
 
 if (require.main === module) {
@@ -16,4 +24,8 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+  // Iterate over the Array, adding each value to a set
+  // Check if the value is in the set already, return that value
+  // return -1 if no dupes found.
 // And a written explanation of your solution
+  // Using a set will provide a quick way to check for duplicates
